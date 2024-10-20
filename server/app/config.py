b05c191fc453bc6e961ae5a5b86c8b1b9a7c0fc3
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-10-19 20:22:03 -0700

    able to get voice output from frontend to backend

--- server/app/config.py
+++ server/app/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-import os 
+import os
+
 # import json
 
 load_dotenv()
@@ -7,4 +8,4 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 JINA_AI_API_KEY = os.getenv("JINA_AI_API_KEY")
-
+DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
