e3d94fcd66de8dcfa771209964a45c34507637d5
Tommy Chen <tchen175@ucsc.edu>
2024-10-20 09:42:49 -0700

    hm

--- server/app/services/hyperbolic_service.py
+++ server/app/services/hyperbolic_service.py
@@ -34,44 +34,38 @@ def invoke_hyperbolic(query, model = "meta-llama/Meta-Llama-3.1-8B-Instruct"):
 
 
 
+import requests
+
+url = "https://api.hyperbolic.xyz/v1/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0Y2hlbjE3NUB1Y3NjLmVkdSIsImlhdCI6MTcyOTMxMTMzOH0.v7V9OLCeBUoY-xB-kpXKQCuH4Q85nDOSNgA8hfgw4cI"
+}
+
 def encode_image(img):
     buffered = BytesIO()
     img.save(buffered, format="PNG")
     encoded_string = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return encoded_string
 
-def hyperbolic_image_query(img):
+def hyperbolic_image_query(img, prompt):
     # img = Image.open("path_to_your_image")
     base64_img = encode_image(img)
 
-    api = "https://api.hyperbolic.xyz/v1/chat/completions"
-    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0Y2hlbjE3NUB1Y3NjLmVkdSIsImlhdCI6MTcyOTMxMTMzOH0.v7V9OLCeBUoY-xB-kpXKQCuH4Q85nDOSNgA8hfgw4cI"
-
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}",
-    }
-
-
-    payload = {
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "What is this image?"},
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_img}"},
-                    },
-                ],
-            }
-        ],
-        "model": "meta-llama/Llama-3.2-90B-Vision-Instruct",
+    data = {
+        "prompt": prompt,
+        "images": [{
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_img}",
+        },
+        }],
+        "model": "meta-llama/Llama-3.2-90B-Vision",
         "max_tokens": 2048,
         "temperature": 0.7,
-        "top_p": 0.9,
+        "top_p": 0.9
     }
-
-    response = requests.post(api, headers=headers, json=payload)
+    
+    response = requests.post(url, headers=headers, json=data)
     print(response.json())
 
